02a422b661c70bfe8134f2ff1c1c351f6b53a0a8
Gabriela01M <alvarenga.2112@gmail.com>
2025-01-15 12:09:48 -0600

    Agregar archivos de proyecto.

---
+++ GridEditinLine.Module/BusinessObjects/DomainObject1.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace GridEditinLine.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class DomainObject1 : BaseObject
+    { 
+        public DomainObject1(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://docs.devexpress.com/eXpressAppFramework/112834/getting-started/in-depth-tutorial-winforms-webforms/business-model-design/initialize-a-property-after-creating-an-object-xpo?v=22.1).
+        }
+
+
+        string phone;
+        int age;
+        DateTime d;
+        string lastName;
+        string firstName;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string FirstName
+        {
+            get => firstName;
+            set => SetPropertyValue(nameof(FirstName), ref firstName, value);
+        }
+
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string LastName
+        {
+            get => lastName;
+            set => SetPropertyValue(nameof(LastName), ref lastName, value);
+        }
+
+
+        public int Age
+        {
+            get => age;
+            set => SetPropertyValue(nameof(Age), ref age, value);
+        }
+
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Phone
+        {
+            get => phone;
+            set => SetPropertyValue(nameof(Phone), ref phone, value);
+        }
+    }
+}
---
+++ GridEditinLine.Module/Controllers/ViewController1.cs
@@ -0,0 +1,143 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Blazor.Editors;
+using Microsoft.AspNetCore.Components;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GridEditinLine.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ViewController.
+    public partial class ViewController1 : ViewController<ListView>
+    {
+        private bool isDoubleClick = false;
+        private bool singleClickHandled = false;
+        private readonly int doubleClickDelay = 300;
+        private bool isHandlingClick = false;
+        private CancellationTokenSource clickCancellationTokenSource;
+        public ViewController1()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+
+            if (View.Editor is DxGridListEditor editor)
+            {
+                editor.RowClickMode = RowClickMode.SelectOnSingleProcessOnDouble;
+
+                var oldRowClick = editor.GridModel.RowClick;
+                var oldRowDoubleClick = editor.GridModel.RowDoubleClick;
+
+                editor.GridModel.RowClick = EventCallback.Factory.Create<GridRowClickEventArgs>(this, async (e) =>
+                {
+                    clickCancellationTokenSource?.Cancel();
+                    clickCancellationTokenSource = new CancellationTokenSource();
+
+                    try
+                    {
+                        await Task.Delay(doubleClickDelay, clickCancellationTokenSource.Token);
+
+                        if (!isDoubleClick)
+                        {
+                            await oldRowClick.InvokeAsync(e);
+
+                            var grid = (DxGrid)e.Grid;
+
+                            if (IsAnyColumnModified())
+                            {
+                                await grid.SaveChangesAsync();
+                            }
+
+                            await StartRowEdit(grid, e.VisibleIndex);
+                        }
+                    }
+                    catch (TaskCanceledException)
+                    {
+                    }
+                    finally
+                    {
+                        isDoubleClick = false;
+                    }
+                });
+
+                editor.GridModel.RowDoubleClick = EventCallback.Factory.Create<GridRowClickEventArgs>(this, async (e) =>
+                {
+                    isDoubleClick = true;
+
+                    clickCancellationTokenSource?.Cancel();
+                    await oldRowDoubleClick.InvokeAsync(e);
+
+                    var grid = (DxGrid)e.Grid;
+                    await grid.SaveChangesAsync();
+
+                    var focusedRowIndex = grid.GetFocusedRowIndex();
+                    var dataItem = ResolveObjectFromFocusedRowIndex(focusedRowIndex);
+
+                    if (dataItem != null)
+                    {
+                        var newObjectSpace = Application.CreateObjectSpace();
+                        var objectInNewSpace = newObjectSpace.GetObject(dataItem);
+                        var detailView = Application.CreateDetailView(newObjectSpace, objectInNewSpace, true);
+
+                        Frame.SetView(detailView);
+                    }
+                });
+            }
+        }
+
+        private object ResolveObjectFromFocusedRowIndex(int focusedRowIndex)
+        {
+            if (focusedRowIndex < 0) return null;
+
+            var collection = View.ObjectSpace.GetObjects(View.ObjectTypeInfo.Type);
+            var enumerableCollection = collection.Cast<object>().ToList();
+
+            if (focusedRowIndex >= 0 && focusedRowIndex < enumerableCollection.Count)
+            {
+                return enumerableCollection[focusedRowIndex];
+            }
+
+            return null;
+        }
+
+        private bool IsAnyColumnModified()
+        {
+            return View.ObjectSpace.IsModified;
+        }
+
+        private object GetObjectFromGrid(int visibleIndex)
+        {
+            var objectSpace = View.ObjectSpace;
+            var collection = View.ObjectSpace.GetObjects(View.ObjectTypeInfo.Type);
+            var enumerableCollection = collection.Cast<object>().ToList();
+
+            if (enumerableCollection != null && enumerableCollection.Count > visibleIndex)
+            {
+                return enumerableCollection.ElementAt(visibleIndex);
+            }
+            else
+                return null;
+        }
+
+        private async Task StartRowEdit(DxGrid grid, int visibleIndex)
+        {
+            await grid.StartEditRowAsync(visibleIndex);
+        }
+
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
